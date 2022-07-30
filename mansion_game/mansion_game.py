3faae36ecde33260dd5c67c206246b18ad27c886
EfraOut <gom21023@byui.edu>
2022-07-30 12:48:46 -0600

    FIX: Some text is joined wrong. Guess game also seems weird

--- mansion_game/mansion_game.py
+++ mansion_game/mansion_game.py
@@ -6,124 +6,10 @@ Teacher: Comeau, Luc
 import random
 import sys
 import time
+from game_text import *
 
 DESCRIPTION_INDEX = 0
 inventory = []
-
-def print_intro():
-    """
-    The intro text for the game
-    """
-    intro = """You've been stranded in a forest for a long time. You can't even remember the last time you ate something besides berries.\n\
-The helicopter that crashed is way behind you, or at least you expected you haven't been walking in circles.
-Far, in the distance, you can see a mansion. Salvation! You sprint towards it, and you get excited as it gets bigger and bigger.\n\
-Immediately after you put your hand on the doorknob, you start hesitating. “What if I get trapped inside?”.\n\
-Your natural impulse doesn't allow you to keep waiting. And so, you enter the mansion.
-Now that you are inside, you want to go out.
-"""
-    return intro
-
-def get_rooms():
-    """
-    Creates the dictionary where the game happens
-    """
-    rooms = {
-        'entrance' : ["""It doesn't look like this mansion is very big. You might as well call it a cottage. You can only see some stairs that lead to the upper floor.\n\
-It also looks like there is a hallway that allows you to keep exploring the main floor.\n\
-Looking more to your left there is a door, that it looks like it leads to the basement, but you can't tell from here.\n\
-A thought comes to your mind “You might as well leave this place, is not worth it”. But do you really want to go back to the berries?""",
-"Main Door" ,'Upper Floor', 'Main Floor', 'Basement'],
-
-        'upper floor' : ["""There are three different rooms you can access from here:\n\
-One of them looks to be the room where the owner of the mansion used to live.\n\
-Next to that room, you can see the library.\n\
-The last room seems to be another bedroom. Perhaps it was for the owner's child?""",
-        "Master's Bedroom", 'Library', "Child's Bedroom", 'Entrance'],
-
-        'main floor' : ["Well, this is very disappointing. You can only see two doors from here.",
-         'Bathroom', "Servant's quarters", 'Entrance'],
-
-        "master's bedroom" : ["""Just as you suspected, this is the owner's room. It looks like nobody's been living in this mansion for days.\n\
-Why would someone build a mansion in the middle of the forest in the first place?\n\
-There is nothing else to see here… oh, wait! Something is moving!""",
-         'Upper Floor', "Investigate"],
-
-        'library' : ["""There are a lot of dusty books. You pick up one of them, open it, but it's impossible to read.\n\
-Dust scatters from the books you close it, confirming to you that it has been a long time since they've been used.\n\
-This rooms connects with a hallway, that gives you the impression to go on eternally; there is another door leading to the child's bedroom, and another one connects to the entire upper floor.\n\
-There also looks to be a big staircase, with multiple branches coming from it.""",
-         "Child's Bedroom", 'Hallway', 'Stairway', 'Upper Floor'],
-
-        "child's bedroom" : ["""This room doesn't give you the best vibes. You can see a cradle and a bed, but there's sign that only one person has lived here.\n\
-“I would go crazy if I had to live on the same room my entire life” This room connects with the entire upper floor.\n\
-There is also a passage to the library, and connects with some big staircase, with multiple branches coming from it.""",
-         'Library', 'Stairway', 'Upper Floor'],
-
-        'stairway' : ["""This stairway is crazy! It is very convenient, since it can connect rooms from both floors!""",
-         "Child's Bedroom", 'Library', "Servant's Quarters", "Hallway", "Kitchen"],
-
-        'hallway' : ["""This hallway is not as endless as you thought. But it is very long considering that you can only access the stairway and the library.\n\
-It also looks like you can access the attic from here.""",
-         'Library', 'Stairway', "Attic"],
-
-        'attic' : ["""This is a very modest attic, with enough room to put Christmas decorations for the whole mansion.\n\
-There seems to be access to the roof from here.""",
-         'Hallway', 'Rooftop'],
-
-        'rooftop' : ["""From here you can see that the forest is bigger than you thought. Looking down gives you a little bit of vertigo, but you can't stop thinking that you could probably make that jump.\n\
-There's also a single horseshoe on the floor.""",
-         'Attic', 'Jump Out', "Grab Horseshoe"],
-
-        "servant's quarters" : ["Probably not a lot of servants served in this mansion. This room just has enough space for two people.\n\
-But hey, at least it connects with the bathroom and a room where they could rest.",
-         'Main Floor', 'Bathroom', 'Break Room'],
-
-        'break room': ["""Probably the place where the servants spent most of their time.\n\
-The couches look wrecked, and the only bookshelf has tons of used books.\n\
-You keep staring at the bookshelf. It looks suspicious. """,
-         "Servant's Quarters", 'Kitchen', 'Move Bookshelf'],
-
-        'secret room': ["""You can't believe your eyes! This place can take you outside the mansion. When you were almost out, a giant turtle stops you. “If you wish to continue, you must give me a horseshoe”.\n\
-“Why would a turtle want a horseshoe?” you wonder. But you keep forgetting the obvious:\n\
-Do you have a horseshoe?""",
-         "Give item"],
-
-        'bathroom':  ["""You can barely call this a bathroom. This small room has just enough space for the toilet and a trash can.\n\
-It looks so uncomfortable to use it that the mere thought of someone trying to use it disturbs you. At least there is another door.""",
-         'Main Floor', "Servant's Quarters"],
-
-        'kitchen' : ["""First thing that you can see is a delicious cake. It looks a lot better than the berries you've been eating.\n\
-This room also connects with the stairway and the break room.""", 
-        "Eat", 'Stairway', 'Break Room'],
-
-        'basement' : """Opening that door that you thought it led to the basement, a chill wind brushes through your body.\n\
-Suddenly, as if the whole room were a vacuum, you can feel a force trying to drag you inside. Without sufficient strength to overcome it, you enter the room, just to realize that you're falling.\n\
-There is nothing but an infinite hole, and now you are eternally falling.""",
-
-        'main door' : """You try to open the door, ready to leave this creepy mansion. The door doesn't budge, so you start pulling the doorknob.\n\
-You can't hold the doorknob with your sweaty hand, and you fall backwards, hitting your head. You die from brain damage.""",
-
-        'investigate' : """As you approach the moving object, you realize that it's not an object, but rather a raccoon.\n\
-The racoon jumps straight at your face and grabs your face. Trying to frere yourself from the racoon, you trip and fall through the window.""", 
-
-        'eat' : """"“You shouldn't eat strange food, you never know what could happen” You remember your mom's advice, but rather late.\n\
-Laying on the floor hasn't been more comfortable before, specially now that you can feel how the air is leaving your lounges.\n\
-It's starting to get cold and dark; you just want to take a nap. You see a tunnel, and there's light at the end. You go towards the light""",
-
-        'jump out' : """You tried to fly away, but gravity is stronger than your will.\n\
-Did you expect something else to happen?""", 
-
-        'good ending' : """You can go on. The turtle is nowhere to be found. You keep wondering why you entered in the first place.\n\
-You start pondering about what you just experienced, and you can't stop but think that nothing that just happened made any sense.\n\
-Maybe you are just a character from a game, made by a BYU Idaho student? Who knows, but if that were true, maybe there is another secret room somewhere inside this crazy mansion.\n\
-Maybe that would make you go inside again, but who knows.\n\
-You decide.""",
-
-        'bad ending' : "Without any attempts left, the room is now so close that you must crouch.\n\
-Without compassion, the room continues collapsing. You make your last wish: To die painlessly.\n\
-I hope your wish came true, my friend.",
-        }
-    return rooms
 
 
 def print_typewriter(message, time_delay_1, time_delay_2):
@@ -145,7 +31,7 @@ def print_typewriter(message, time_delay_1, time_delay_2):
             time.sleep(time_delay_1)
         else:
             time.sleep(time_delay_2)
-    print() # Returns pointer to the bottom
+    print()  # Returns pointer to the bottom
 
 
 def explore_mansion(room_list):
@@ -182,7 +68,9 @@ def check_game_over(room, rooms_dict):
     """
     alive = True
 
-    if room in ('basement', 'main door', 'investigate', 'eat', 'jump out', 'good ending', 'bad ending'):
+    if room in (
+        'basement', 'main door', 'investigate', 'eat', 'jump out',
+        'good ending', 'bad ending'):
         print_typewriter(rooms_dict[room], 0.05, 0.05)
         alive = False
 
@@ -199,23 +87,33 @@ def apply_special_conditions(room, room_list):
     Return: The room where the player should be after the special
             condition finishes
     """
-    if room == 'move bookshelf': # Finding the Secret Room
-        print_typewriter('At the other side of the bookshelf, where a wall should be, there is now a secret room', 0.01, 0.05)
+
+    # Finding the Secret Room
+    if room == 'move bookshelf': 
+        print_typewriter(
+            'At the other side of the bookshelf, where a wall should be,'
+             + 'there is now a secret room', 0.01, 0.05)
         room_list.pop()
         room_list.append('Secret Room')
-        room_list[DESCRIPTION_INDEX] = "Aja! You knew there was something weird with that bookshelf. But, what now?"
+        room_list[DESCRIPTION_INDEX] = "Aja! You knew there was something" \
+                                + "weird with that bookshelf. But, what now?"
         room = 'break room'
-    elif room == 'grab horseshoe': # Adding 'Horseshoe' to the inventory
+
+    # Adding 'Horseshoe' to the inventory
+    elif room == 'grab horseshoe':  
         print_typewriter('You grabbed the horseshoe', 0.1, 0.1)
         inventory.append('Horseshoe')
         room_list.pop()
-        room_list[DESCRIPTION_INDEX] = "From here you can see that the forest is bigger than you thought.\n\
-Looking down gives you a little bit of vertigo, but you can't stop thinking that you could probably make that jump."
+        room_list[DESCRIPTION_INDEX] = "From here you can see that the forest" \
+            + " is bigger than you thought.\n\ Looking down gives you a little bit" \
+            + "of vertigo, but you can't stop thinking that you could probably make that jump."
         room = 'rooftop'
-    elif room == 'give item': # Giving the item
-        if inventory == ['Horseshoe']: # Guess My Number begins
+
+    # Giving the item for the final game to begin.
+    elif room == 'give item':  
+        if inventory == ['Horseshoe']:  # Guess My Number begins
             room = guess_number()
-        else: # Allowing the player to find the item
+        else:  # Allowing the player to find the item
             print("Bring me the horseshoe, and then we can talk")
             room = 'break room'
 
@@ -229,7 +127,9 @@ def guess_number():
     """
     while True:
         try:
-            print_typewriter('Alright, now that I have my horseshoe, try to guess my number. It is between 1 and 10', 0.01, 0.05)
+            print_typewriter(
+                'Alright, now that I have my horseshoe, try to guess my number.' \
+                 + 'It is between 1 and 10', 0.01, 0.05)
             attempts = 1
             number = random.randint(1, 10)
             guess = int(input("Enter number "))
@@ -243,7 +143,8 @@ def guess_number():
                     guess = int(input("Enter number "))
                     attempts += 1
                 elif guess == 42:
-                    print_typewriter('Hi! My name is Efrain. Thank you for playing!', 0.5, 0.5)
+                    print_typewriter(
+                        'Hi! My name is Efrain. Thank you for playing!', 0.5, 0.5)
             if guess == number:
                 room = 'good ending'
                 break
@@ -254,26 +155,27 @@ def guess_number():
             print('Enter valid number')
 
     return room
-        
+
 
 def main():
     print_typewriter(print_intro(), 0.01, 0.05)
     alive = True
-    rooms = get_rooms() # This variable stores the dictionary
-    room_list = rooms['entrance'] # You have to start somewhere, right?
+    rooms = get_rooms()  # This variable stores the dictionary
+    room_list = rooms['entrance']  # You have to start somewhere, right?
     while alive:
         try:
             room_key = explore_mansion(room_list)
 
-            # Some rooms have special conditions (grabbing an item, giving an item or moving something)
-            # This function takes care of those conditions
+            # Some rooms have special conditions, like grabbing an item,
+            # giving an item or moving something.3
+            # This algorithm takes care of those conditions.
             if room_key in ('grab horseshoe', 'move bookshelf', 'give item'):
                 room_key = apply_special_conditions(room_key, room_list)
 
             # Get the item for the dictionary to be used continuing the loop
             room_list = rooms[room_key]
-            
-            # Cheking if the game is over
+
+            # Checking if the game is over
             alive = check_game_over(room_key, rooms)
 
         except ValueError:
